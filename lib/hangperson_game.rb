107ef69d6edf23f21e4e28f912995e512c230778
jonkim93 <jonkim93@gmail.com>
2015-02-06 17:51:03 -0400

    partway through implementing game functionality

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,6 +4,30 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
+
+  def initialize(word)
+  	@word = word
+  	@guesses = ''
+  	@wrong_guesses = ''
+  	@word_with_guesses = ''
+  	word.length.times {@word_with_guesses+='-'}
+  end
+
+  def guess(guess)
+  	if guess=='' || !(guess =~ /[A-Za-z]/) || guess==nil
+  		raise ArgumentError
+  	elsif @word.include? guess 
+  		occurrences = (0 ... word.length).find_all { |i| word[i] == guess }
+  		occurrences.each {|i| @word_with_guesses[i]=guess}
+  		@guesses += guess if !@guesses.include? guess
+  	else
+  		@wrong_guesses += guess if !@wrong_guesses.include? guess
+  	end
+  end
 
   def self.get_random_word
     require 'uri'
